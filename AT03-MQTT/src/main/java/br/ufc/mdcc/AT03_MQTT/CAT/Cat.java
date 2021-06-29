a3bc45a0cf458fa754239a15125fc34ddc3c43be
Marcos <m@m.com>
2021-06-29 19:15:13 -0300

    Versão inicial

---
+++ AT03-MQTT/src/main/java/br/ufc/mdcc/AT03_MQTT/CAT/Cat.java
@@ -0,0 +1,9 @@
+package br.ufc.mdcc.AT03_MQTT.CAT;
+
+public class Cat {
+
+	public static void main(String[] args) {
+		System.out.println("Teste");
+	}
+
+}
---
+++ AT03-MQTT/src/main/java/br/ufc/mdcc/AT03_MQTT/sensor/SimuladorSensores.java
@@ -0,0 +1,9 @@
+package br.ufc.mdcc.AT03_MQTT.sensor;
+
+public class SimuladorSensores {
+
+	public static void main(String[] args) {
+		System.out.println("Teste");
+	}
+
+}
